4dcec9a5c9efbfb95c70239957414b77ff921c5e
Yirang Tiffany Arellano <yirang@mostlyserious.io>
2021-01-24 12:54:03 -0500

    Generate new key and remove from settings.py

--- wisdompets/wisdompets/settings.py
+++ wisdompets/wisdompets/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4b*a=s-$ehoo**^lf*&ie^e@e_@rdq)1ic7c&r!1gtn_^s8$i-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
